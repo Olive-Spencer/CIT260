19b1274a243cc100328b177a8e03b82b81a3fd1d
Olive-Spencer <35235065+Olive-Spencer@users.noreply.github.com>
2018-02-13 21:22:23 -0700

    Lesson 6 Group Assignment

---
+++ OregonTrail/src/oregontrail/control/TrailControl.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oregontrail.control;
+import java.util.Random;
+
+
+
+/**
+ *
+ * @author spencer
+ */
+public class TrailControl {
+    public static Random rand = new Random();
+    
+    
+    
+    public static double calcWeather(int locationCondition, int month){
+        int weatherMonth=0;
+        
+        if (month < 0 || month > 11){ 
+            return -1;
+        }
+        if (locationCondition < 0 || locationCondition > 2){
+            return -1;
+        }
+if (month == 10 || month == 11 || month == 0 || month == 1){
+    weatherMonth = 0;
+            }
+else if (month == 5 || month == 6 || month == 7)
+{
+    weatherMonth = 5;
+            }
+else{ 
+    weatherMonth = 10;
+}
+double randomNumber = rand.nextInt(10);
+
+if (randomNumber < 0 || randomNumber > 10){
+    return -1;
+}
+        
+double weatherStatus = locationCondition + weatherMonth + (10/ 10);
+
+ return weatherStatus;
+
+    }
+    
+}
---
+++ OregonTrail/test/oregontrail/control/TrailControlTest.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oregontrail.control;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Spencer, Angie, Coby
+ */
+public class TrailControlTest {
+    
+    public TrailControlTest() {
+    }
+
+    /**
+     * Test of calcWeather method, of class TrailControl.
+     */
+    @Test
+    public void testCalcWeather() {
+        System.out.println("test case boundry 1");
+        int locationCondition = 0;
+        int month = 0;
+        double expResult = 1.0;
+        double result = TrailControl.calcWeather(locationCondition, month);
+        assertEquals(expResult, result, 0.0);
+        
+        System.out.println("test case valid 1");
+        locationCondition = 1;
+        month = 10;
+        expResult = 2.0;
+        result = TrailControl.calcWeather(locationCondition, month);
+        assertEquals(expResult, result, 0.0);
+        
+        System.out.println("test case boundry 2");
+        locationCondition = 2;
+        month = 11;
+        expResult = 3.0;
+        result = TrailControl.calcWeather(locationCondition, month);
+        assertEquals(expResult, result, 0.0);
+        
+        System.out.println("test case invalid 1");
+        locationCondition = -1;
+        month = 0;
+        expResult = -1.0;
+        result = TrailControl.calcWeather(locationCondition, month);
+        assertEquals(expResult, result, 0.0);
+        
+        System.out.println("test case invalid 2");
+        locationCondition = 1;
+        month = 13;
+        expResult = -1.0;
+        result = TrailControl.calcWeather(locationCondition, month);
+        assertEquals(expResult, result, 0.0);
+        
+    }
+    
+}
